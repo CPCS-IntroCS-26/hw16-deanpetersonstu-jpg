5467d9a86335724f1a6b2f4bfa5181ac034fbb1f
deanpetersonstu-jpg <dean.peterson.stu@cedarpark.org>
2026-05-26 09:38:58 -0700

    Final Submit

--- animals.py
+++ animals.py
@@ -1,53 +1,129 @@
 class Animal:
     def __init__(self, name, age, sound):
-        pass
+        self.name = name
+        self.age = age
+        self.sound = sound
 
     def speak(self):
-        pass
+        print(f"{Animal.name} says Grrr")
 
     def move(self):
-        pass
+        print(f"{Animal.name} Runs")
 
     def describe(self):
-        pass
+        print(f"{Animal.name}: is a {Animal.age} that makes this sound: {Animal.sound}")
 
     def __str__(self):
-        pass
+        return f'name: {self.name}\n' \
+        f'age: {self.age}\n' \
+        f'breed: {self.sound}'
 
 
 class Dog(Animal):
     def __init__(self, name, age, breed):
-        pass
-
+        super().__init__(name, age, "Woof!")
+        self.breed = breed    
+    
     def speak(self):
-        pass
+        print(f"{self.name} says Woof")
 
     def move(self):
-        pass
+        print(f"{self.name} sprints")
+
+    def describe(self):
+        print(f"{self.name} is a {self.age} year old {self.breed}")
+
+    def __str__(self):
+        return f'name: {self.name}\n' \
+        f'age: {self.age}\n' \
+        f'breed: {self.breed}'
 
 
 class Bird(Animal):
-    def __init__(self, name, age, can_fly):
-        pass
+    def __init__(self, name, age, beak):
+        super().__init__(name, age, "Chirp")
+        self.beak = beak    
+    
+    def speak(self):
+        print(f"{self.name} chirps")
 
     def move(self):
-        pass
+        print(f"{self.name} flys")
+
+    def describe(self):
+        print(f"{self.name} is a {self.age} year old bird with a {self.beak}")
+
+    def __str__(self):
+        return f'name: {self.name}\n' \
+        f'age: {self.age}\n' \
+        f'beak: {self.beak}'
 
 
 class Fish(Animal):
     def __init__(self, name, age, water_type):
-        pass
+        super().__init__(name, age, "Bubble")
+        self.water_type = water_type    
+    
+    def speak(self):
+        print(f"{self.name} says bubble")
 
     def move(self):
-        pass
+        print(f"{self.name} swims")
+
+    def describe(self):
+        print(f"{self.name} is a {self.age} year old fish that lives in {self.water_type}")
+
+    def __str__(self):
+        return f'name: {self.name}\n' \
+        f'age: {self.age}\n' \
+        f'water_type: {self.water_type}'
 
 
 class Cat(Animal):
     def __init__(self, name, age, indoor):
-        pass
-
+        super().__init__(name, age, "meow")
+        self.indoor = indoor    
+    
     def speak(self):
-        pass
+        print(f"{self.name} says meow")
 
     def move(self):
-        pass
+        print(f"{self.name} walks")
+
+    def describe(self):
+        print(f"{self.name} is a {self.age} year old cat that is an {self.indoor}")
+
+    def __str__(self):
+        return f'name: {self.name}\n' \
+        f'age: {self.age}\n' \
+        f'indoor: {self.indoor}'
+
+"""
+class Contact:
+    def __init__(self, name, phone, email):
+        self.__name = name
+        self.__phone = phone
+        self.__email = email
+
+    def set_name(self, name):
+       self.__name = name
+
+    def set_phone(self, phone):
+       self.__phone = phone
+
+    def set_email(self, email):
+       self.__email = email
+
+    def get_name(self):
+        return self.__name
+    
+    def get_phone(self):
+        return self.__phone
+    
+    def get_email(self):
+        return self.__email
+
+    def __str__(self):
+        return f'name: {self.__name}\n' \
+               f'name: {self.__phone}\n' \
+               f'name: {self.__email}'"""
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from animals import Animal, Dog, Bird, Fish, Cat
 
-
 def main():
     # Create one instance of each animal subclass
-    animals = []
+    animals = [
+        Dog("Jeff", "12", "Golden Retriever"),
+        Bird("Mark","10","long Beak"),
+        Fish("Dave", "4", "fresh Water"),
+        Cat("Maisie", "5", "indoor cat")
+    ]
 
     # TODO: instantiate your animals and add them to the list
 
     # Loop over all animals and call speak(), move(), and describe()
     for animal in animals:
-        pass
+        animal.speak()
+        animal.move()
+        animal.describe()
+        print()
 
 
 if __name__ == "__main__":
